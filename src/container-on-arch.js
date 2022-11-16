65f0ef73a6dbea1b41407b037edbce1ee24e5c42
vdo <vdo@greyfaze.net>
2022-11-16 14:57:12 +0000

    remove installShell

--- src/container-on-arch.js
+++ src/container-on-arch.js
@@ -49,15 +49,15 @@ async function main() {
   // }
 
   // Write install commands to a script file for running in the Dockerfile
-  const install = [
-    `#!${installShell}`, 'set -eu;', 'export DEBIAN_FRONTEND=noninteractive;',
-    core.getInput('install'),
-  ].join('\n');
-  fs.writeFileSync(
-    // Must be in same directory as Dockerfiles
-    path.join(__dirname, '..', 'Dockerfiles', 'container-on-arch-install.sh'),
-    install,
-  );
+  // const install = [
+  //   `#!${installShell}`, 'set -eu;', 'export DEBIAN_FRONTEND=noninteractive;',
+  //   core.getInput('install'),
+  // ].join('\n');
+  // fs.writeFileSync(
+  //   // Must be in same directory as Dockerfiles
+  //   path.join(__dirname, '..', 'Dockerfiles', 'container-on-arch-install.sh'),
+  //   install,
+  // );
 
   // Write container commands to a script file for running
   const commands = [
